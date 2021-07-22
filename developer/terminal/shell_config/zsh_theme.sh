eb9860b3f2da7c46f9edff77d26a615ce9be2ab4
Arpit Namdeo <arpitnamdeo@gmail.com>
2021-07-22 11:31:47 -0400

    update for theme

--- developer/terminal/shell_config/zsh_theme.sh
+++ developer/terminal/shell_config/zsh_theme.sh
@@ -1,9 +1,6 @@
 ### PowerLevel9K Theme load everywhere but in Ubuntu profile
-source "${HOME}/.shell_config/powerlevel9krc.sh"
-
-## Following shows usage of different prompt themes based on iTerm profile or just use one across the board
-# if [[ $ITERM_PROFILE != "Ubuntu" ]]; then
-#  source "${HOME}/.shell_config/powerlevel9krc.sh"
-#else
-#  source "${HOME}/.shell_config/zshrc_simple_prompt.sh"
-#fi
+if [[ $LC_TERMINAL = "iTerm2" && $ITERM_PROFILE != "Ubuntu" ]]; then
+  source "${HOME}/.shell_config/powerlevel9krc.sh"
+else
+  source "${HOME}/.shell_config/zshrc_ubuntu.sh"
+fi
